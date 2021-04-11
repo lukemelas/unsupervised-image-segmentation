061ee96554b70775c7d736fb31549e0326f03802
Luke <luke.melas@gmail.com>
2021-04-11 18:40:33 -0400

    Update website

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d020bd91ee9a1d31064"; }
+/******/ 		__webpack_require__.h = function() { return "f779419e656fe3322abc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
